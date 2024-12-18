b8a543d084541ad2cdcdaad47abcf01c46ae07bc
Lorenzo Bergami <lorebergam@gmail.com>
2024-12-18 18:51:19 +0100

    prroject initialization

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'bootstrap.php';
+
+//Base Template
+$templateParams["titolo"] = "MindBazaar";
+//Home Template
+
+require 'template/base.php';
+?>
---
+++ template/base.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo $templateParams["titolo"]; ?></title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+</head>
+<body>
+    <header>
+    </header>
+    <nav>
+    </nav>
+    <main>
+    </main><aside>
+    </aside>
+    <footer>
+    </footer>
+</body>
+</html>
