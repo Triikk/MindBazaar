6e395f29b9c3133733fda247801eb4f07c111ee0
Lorenzo Bergami <lorebergam@gmail.com>
2024-12-18 21:49:36 +0100

    added products in MBPopulate

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,14 @@
 <?php
 session_start();
+
 define("UPLOAD_DIR", "./upload/");
 define("CATEGORIES_DIR", UPLOAD_DIR . "categories/");
 define("PRODUCTS_DIR", UPLOAD_DIR . "products/");
+define("DREAMS_DIR", PRODUCTS_DIR . "sogni/");
+define("NOTIONS_DIR", PRODUCTS_DIR . "nozioni/");
+define("INSPIRATIONS_DIR", PRODUCTS_DIR . "ispirazioni/");
+define("EMOTIONS_DIR", PRODUCTS_DIR . "emozioni/");
+
 // require_once("utils/functions.php");
 require_once("db/database.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "mindbazaar", 3306);
