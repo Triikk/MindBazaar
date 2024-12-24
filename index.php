bea3a940f2fbb6cb9a8635b1cf2d186a59847957
Lorenzo Bergami <lorebergam@gmail.com>
2024-12-24 15:27:08 +0100

    removed default user loggin in index

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ $templateParams["titolo"] = "MindBazaar";
 $templateParams["nome"] = "homepage.php";
 $templateParams["bestSellers"] = $dbh->getBestSellers(1);
 
-$_SESSION["username"] = "MATT DESTROYER";
-
 session_destroy();
 
 require 'template/base.php';
