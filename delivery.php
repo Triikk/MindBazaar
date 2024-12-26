6dc9fcd1e62fd02d2cb4597559fd2c8f16657c34
Lorenzo Bergami <lorebergam@gmail.com>
2024-12-26 16:32:46 +0100

    pagina del punto di consegna

---
+++ delivery.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'bootstrap.php';
+
+$templateParams["titolo"] = "MindBazaar - Punto di consegna";
+$templateParams["nome"] = "deliveryContent.php";
+
+require 'template/base.php';
