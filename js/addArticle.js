7338c2a9a4f6f9417148d68dd76cdaf37a4b952c
Mattia Ronchi <mattia.ronchi5@studio.unibo.it>
2024-12-28 22:29:08 +0100

    refactoring (generalised the web api request)

--- js/addArticle.js
+++ js/addArticle.js
@@ -1,23 +1,9 @@
+const API_URL = "api/api-articles.php";
+
 async function addArticle() {
-    const url = `api/api-articles.php`;
+    generateXHttpRequestFromForm(API_URL, "addArticle", document.forms["add-article"]);
+}
 
-    const form = document.forms["add-article"];
-    const formData = new FormData(form);
-
-    const params = {};
-    for (const pair of formData.entries()) {
-        params[pair[0]] = pair[1];
-    }
-
-    try {
-        const xhttp = new XMLHttpRequest();
-        xhttp.open("POST", url, true);
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.onload = function () {
-            alert(JSON.parse(xhttp.responseText)["message"]);
-        };
-        xhttp.send(`query=addArticle&id_prodotto=${params["id_prodotto"]}&formato=${params["formato"]}&durata=${params["durata"]}&disponibilita=${params["disponibilita"]}&prezzo=${params["prezzo"]}&intensita=${params["intensita"]}&versione=${params["versione"]}`);
-    } catch (error) {
-        console.log(error.message);
-    }
+async function addProduct() {
+    generateXHttpRequestFromForm(API_URL, "addProduct", document.forms["add-product"]);
 }
--- js/functions.js
+++ js/functions.js
@@ -5,3 +5,28 @@ function calculateTotal(articles) {
     }
     return total.toFixed(2);
 }
+
+
+/**
+ * Genera una richiesta HTTP POST
+ */
+function generateXHttpRequestFromForm(url, query, form) {
+    const formData = new FormData(form);
+    const params = {};
+    for (const pair of formData.entries()) {
+        params[pair[0]] = pair[1];
+    }
+
+    try {
+        const xhttp = new XMLHttpRequest();
+        xhttp.open("POST", url, true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.onload = function () {
+            alert(JSON.parse(xhttp.responseText)["message"]);
+        };
+        const queryString = new URLSearchParams(params).toString();
+        xhttp.send(`query=${query}&${queryString}`);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
