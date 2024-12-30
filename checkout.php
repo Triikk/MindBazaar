ed3b382458445aa898a48f519fa11e128e8ff55d
Francesco Bittasi <francesco.bittasi@studio.unibo.it>
2024-12-30 11:53:22 +0100

    checkout with alert and redirect

--- checkout.php
+++ checkout.php
@@ -10,13 +10,23 @@ if (isset($_REQUEST["submit"]) && $_REQUEST["submit"] == "ordina") {
 } else if (isset($_REQUEST["submit"]) && $_REQUEST["submit"] == "checkout") {
     $articoliAcquistati = json_decode($_REQUEST["boughtArticles"], true);
     $success = $dbh->checkout($_SESSION["username"], $articoliAcquistati);
-    if ($success) {
-        // alert ?
-        header("location: orders.php");
-    } else {
-        // alert ?
-        header("location: cart.php");
-    }
+    if ($success) : ?>
+        <form id="redirect" action="orders.php" method="post">
+            <input type="hidden" name="ordineEffettuato" value="true">
+        </form>
+        <script type="text/javascript">
+            alert("Ordine effettuato con successo!!!");
+            document.getElementById('redirect').submit();
+        </script>
+    <?php else : ?>
+        <form id="redirect" action="cart.php" method="post">
+            <input type="hidden" name="ordineEffettuato" value="false">
+        </form>
+        <script type="text/javascript">
+            alert("Qualcosa è andato storto: ordine non effettuato");
+            document.getElementById('redirect').submit();
+        </script>
+    <?php endif; 
 }
 
 //Base Template
@@ -25,3 +35,5 @@ $templateParams["titolo"] = "MindBazaar - Checkout";
 $templateParams["nome"] = "orderCheckout.php";
 
 require 'template/base.php';
+
+?>
