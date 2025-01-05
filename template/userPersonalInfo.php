c91233f3e79d683e6071f10ecee39379a7a88cf4
Lorenzo Bergami <lorebergam@gmail.com>
2025-01-05 23:41:39 +0100

    Revert "modified personal info visualization"
    This reverts commit 23e2270357b3a6c99fcaf6325c2404c73fa35263.

--- template/userPersonalInfo.php
+++ template/userPersonalInfo.php
@@ -1,40 +1,17 @@
-<section class="container my-4">
-    <?php $datiUtente = $userParams["datiUtente"][0]; ?>
-    <div class="card mx-auto" style="max-width: 600px;">
-        <div class="card-body">
-            <h2 class="card-title text-center mb-4">Dati Utente</h2>
-            <div class="row mb-3">
-                <div class="col-4">
-                    <h5>Username:</h5>
-                </div>
-                <div class="col-8">
-                    <p class="card-text"><?php echo $datiUtente["username"]; ?></p>
-                </div>
-            </div>
-            <div class="row mb-3">
-                <div class="col-4">
-                    <h5>Nome:</h5>
-                </div>
-                <div class="col-8">
-                    <p class="card-text"><?php echo $datiUtente["nome"]; ?></p>
-                </div>
-            </div>
-            <div class="row mb-3">
-                <div class="col-4">
-                    <h5>Cognome:</h5>
-                </div>
-                <div class="col-8">
-                    <p class="card-text"><?php echo $datiUtente["cognome"]; ?></p>
-                </div>
-            </div>
-            <div class="row mb-3">
-                <div class="col-4">
-                    <h5>Data di nascita:</h5>
-                </div>
-                <div class="col-8">
-                    <p class="card-text"><?php echo $datiUtente["data_nascita"]; ?></p>
-                </div>
-            </div>
-        </div>
-    </div>
+<section>
+    <ul>
+        <?php $datiUtente = $userParams["datiUtente"][0]; ?>
+        <li>
+            <h3>Username: <?php echo $datiUtente["username"] ?></h3>
+        </li>
+        <li>
+            <h3>Nome: <?php echo $datiUtente["nome"] ?></h3>
+        </li>
+        <li>
+            <h3>Cognome: <?php echo $datiUtente["cognome"] ?></h3>
+        </li>
+        <li>
+            <h3>Data di nascita: <?php echo $datiUtente["data_nascita"] ?></h3>
+        </li>
+    </ul>
 </section>
