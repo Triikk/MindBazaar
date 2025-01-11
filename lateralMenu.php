b213fe0ea50001b3f8e649f97bce9809e4778134
Francesco Bittasi <francesco.bittasi@studio.unibo.it>
2025-01-11 10:49:22 +0100

    Removed unused pages

--- lateralMenu.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'bootstrap.php';
-
-//Base Template
-$templateParams["titolo"] = "MindBazaar";
-//Home Template
-$templateParams["nome"] = "lateral.php";
-
-require 'template/base.php';
