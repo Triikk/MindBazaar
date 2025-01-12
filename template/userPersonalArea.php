428598e6e6a847c7d7f75f148a83a00c862964bc
Lorenzo Bergami <lorebergam@gmail.com>
2025-01-12 21:13:13 +0100

    restyled personal area

--- template/userPersonalArea.php
+++ template/userPersonalArea.php
@@ -1,16 +1,16 @@
-<div class="container">
+<div class="container text-center">
     <div class="card mx-auto my-4 personalInfo">
         <div class="card-body">
             <section>
-                <ul>
-                    <li>
+                <ul class="list-unstyled">
+                    <li class="mb-4">
                         <a href="orders.php">I miei ordini</a>
                     </li>
-                    <li>
+                    <li class="mb-4">
                         <a href="personalInfo.php">Informazioni personali</a>
                     </li>
                     <li>
-                        <a href="logout.php">Esci</a>
+                        <a href="logout.php" class="text-danger">Esci</a>
                     </li>
                 </ul>
             </section>
