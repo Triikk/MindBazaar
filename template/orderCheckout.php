03cd3c0bc85406b7b1d24e8f63e01031fe6e2dde
Lorenzo Bergami <lorebergam@gmail.com>
2025-01-14 18:52:04 +0100

    changed checkout button color

--- template/orderCheckout.php
+++ template/orderCheckout.php
@@ -1,34 +1,34 @@
 <section>
-<div class="col-12 col-md-8 mb-4 mx-auto">
-<div class="card d-flex flex-column position-relative cartArticleCard">
-    <div class="card-body">
-        <h2 class="card-title">Dettagli dell'ordine</h2>
-        <p class="card-subtitle pb-3"><?php echo(count($listaArticoli)) ?> articol<?php
-        if (count($listaArticoli) > 1) {
-            echo("i");
-        } else {
-            echo("o");
-        } ?> nell'ordine</p>
-        <p><b>Totale: <?php echo(calculateTotal($listaArticoli)) ?>€</b></p>
-        <form id="checkout-form" action="checkout.php" method="post" onsubmit='checkout()'>
-            <input form="checkout-form" type="submit" name="submit" value="checkout" class="btn btn-success btn-lg btn-block w-100" />
-            <input form="checkout-form" type="hidden" name="boughtArticles" value='<?php echo(json_encode($listaArticoli, JSON_HEX_APOS | JSON_HEX_QUOT)) ?>' />
-        </form>
+    <div class="col-12 col-md-8 mb-4 mx-auto">
+        <div class="card d-flex flex-column position-relative cartArticleCard">
+            <div class="card-body">
+                <h2 class="card-title">Dettagli dell'ordine</h2>
+                <p class="card-subtitle pb-3"><?php echo (count($listaArticoli)) ?> articol<?php
+                                                                                            if (count($listaArticoli) > 1) {
+                                                                                                echo ("i");
+                                                                                            } else {
+                                                                                                echo ("o");
+                                                                                            } ?> nell'ordine</p>
+                <p><b>Totale: <?php echo (calculateTotal($listaArticoli)) ?>€</b></p>
+                <form id="checkout-form" action="checkout.php" method="post" onsubmit='checkout()'>
+                    <input form="checkout-form" type="submit" name="submit" value="checkout" class="btn btn-success btn-lg btn-block w-100 clickable" />
+                    <input form="checkout-form" type="hidden" name="boughtArticles" value='<?php echo (json_encode($listaArticoli, JSON_HEX_APOS | JSON_HEX_QUOT)) ?>' />
+                </form>
+            </div>
+        </div>
     </div>
-</div>
-</div>
 </section>
 <section>
-<div class="col-12 col-md-8 mb-4 mx-auto">
-    <?php foreach ($listaArticoli as $articolo): ?>
-        <div class="card mb-2 d-flex flex-column position-relative cartArticleCard">
-        <div class="card-body">
-            <h3 class="card-title"><?php echo($articolo["nome"]) ?></h3>
-            <p class="card-subtitle pb-3">Quantità: <?php echo($articolo["quantita"]) ?></p>
-            <p class="card-text">Prezzo: <?php echo($articolo["prezzo"]) ?>€</p>
-            <p class="card-text">Prezzo parziale: <?php echo($articolo["prezzo"] * $articolo["quantita"]) ?>€</p>
-        </div>
-        </div>
-    <?php endforeach; ?>
-</div>
+    <div class="col-12 col-md-8 mb-4 mx-auto">
+        <?php foreach ($listaArticoli as $articolo): ?>
+            <div class="card mb-2 d-flex flex-column position-relative cartArticleCard">
+                <div class="card-body">
+                    <h3 class="card-title"><?php echo ($articolo["nome"]) ?></h3>
+                    <p class="card-subtitle pb-3">Quantità: <?php echo ($articolo["quantita"]) ?></p>
+                    <p class="card-text">Prezzo: <?php echo ($articolo["prezzo"]) ?>€</p>
+                    <p class="card-text">Prezzo parziale: <?php echo ($articolo["prezzo"] * $articolo["quantita"]) ?>€</p>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </section>
